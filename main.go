c2db609c1a19103c4725c3f7c55b0299e771e048
sfod <octosyllabic@gmail.com>
2019-09-28 11:48:30 +0300

    Add a few more paths

--- main.go
+++ main.go
@@ -6,12 +6,27 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+    if r.URL.Path != "/" {
+        http.NotFound(w, r)
+        return
+    }
     w.Write([]byte("Hi you"))
+}
+
+func show(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("show"))
+}
+
+func create(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("create"))
 }
 
 func main() {
     mux := http.NewServeMux()
+
     mux.HandleFunc("/", home)
+    mux.HandleFunc("/snippet/show", show)
+    mux.HandleFunc("/snippet/create", create)
 
     log.Println("Starting server on :4000")
     err := http.ListenAndServe(":4000", mux)
