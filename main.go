e199639e80824118c338f81a810a4238b3c2c809
sfod <octosyllabic@gmail.com>
2019-09-28 13:17:34 +0300

    Request snippet by id

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+    "fmt"
     "log"
     "net/http"
+    "strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -14,7 +16,12 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func show(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("show"))
+    id, err := strconv.Atoi(r.URL.Query().Get("id"))
+    if err != nil || id < 1 {
+        http.NotFound(w, r)
+        return
+    }
+    fmt.Fprintf(w, "Display snippet with ID %d", id)
 }
 
 func create(w http.ResponseWriter, r *http.Request) {
