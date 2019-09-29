32a8e5b271779ea58373560ff83eada383fbecb8
sfod <octosyllabic@gmail.com>
2019-09-29 15:23:04 +0300

    Add static content
    From alexedwards.net

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -12,6 +12,9 @@ func main() {
     mux.HandleFunc("/snippet/show", show)
     mux.HandleFunc("/snippet/create", create)
 
+    fileServer := http.FileServer(http.Dir("./ui/static/"))
+    mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
     log.Println("Starting server on :4000")
     err := http.ListenAndServe(":4000", mux)
     log.Fatal(err)
